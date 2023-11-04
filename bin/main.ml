574797f9c72d9c0f18dd89395775050fc659358c
AngelVI <a.v.iliev13@gmail.com>
2023-11-04 14:47:22 +0200

    initial version of make_move

--- bin/main.ml
+++ bin/main.ml
@@ -1,19 +1,34 @@
 module Board = struct
-  type player = PlayerX | (* PlayerO | *) NoPlayer
-  type board = { player_to_move : player; state : player list }
+  type player = PlayerX | PlayerO | NoPlayer
+  type board = { player_to_move : player; state : player list; row_size : int }
 
   let new_ row_size =
     let state = List.init (row_size * row_size) (function _ -> NoPlayer) in
-    { player_to_move = PlayerX; state }
+    { player_to_move = PlayerX; state; row_size }
+
+  (* TODO: check if move in range *)
+  let make_move move board =
+    let state =
+      List.mapi
+        (fun i v -> if i == move then board.player_to_move else v)
+        board.state
+    in
+    let player_to_move =
+      match board.player_to_move with
+      | PlayerX -> PlayerO
+      | PlayerO -> PlayerX
+      | NoPlayer -> PlayerX
+    in
+    { board with state; player_to_move }
 end
 
 let row_size = 3
 
 let () =
   let b = Board.new_ row_size in
-  let x = match b.player_to_move with PlayerX -> -1 | NoPlayer -> 0 in
-  print_int x;
   print_newline ();
   (* TODO: how to print PlayerX & others, also what values do they carry ? *)
   print_int @@ List.length b.state;
+  print_newline ();
+  print_int b.row_size;
   print_newline ()
