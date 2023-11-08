896252ac46d22b839d1929a2e8e5b65072ed421f
AngelVI <a.v.iliev13@gmail.com>
2023-11-08 15:36:31 +0200

    calculate winner

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,14 @@
 module Board = struct
   type player = PlayerX | PlayerO | NoPlayer [@@deriving show]
 
+  let int_of_player = function PlayerX -> -1 | PlayerO -> 1 | NoPlayer -> 0
+  let player_of_int = function -1 -> PlayerX | 0 -> PlayerO | _ -> NoPlayer
+
+  let opposite_player = function
+    | PlayerX -> PlayerO
+    | PlayerO -> PlayerX
+    | _ -> NoPlayer
+
   type board = { player_to_move : player; state : player list; row_size : int }
   [@@deriving show]
 
@@ -23,44 +31,65 @@ module Board = struct
         (fun i v -> if i == move then board.player_to_move else v)
         board.state
     in
-    let player_to_move =
-      match board.player_to_move with
-      | PlayerX -> PlayerO
-      | PlayerO -> PlayerX
-      | NoPlayer -> failwith "unexpected player to move: NoPlayer"
-    in
+    let player_to_move = opposite_player board.player_to_move in
     { board with state; player_to_move }
 
   let compute_winning_lines _board =
-    [[0;1;2];[3;4;5];[6;7;8];[0;3;6];[1;4;7];[2;5;8];[0;4;8];[2;4;6]]
+    [
+      [ 0; 1; 2 ];
+      [ 3; 4; 5 ];
+      [ 6; 7; 8 ];
+      [ 0; 3; 6 ];
+      [ 1; 4; 7 ];
+      [ 2; 5; 8 ];
+      [ 0; 4; 8 ];
+      [ 2; 4; 6 ];
+    ]
 
   let rec check_line_aux board score line =
-    match line with 
-    | [] -> ( match abs(score)/board.row_size with | -1 -> PlayerX | 1 -> PlayerO | _ -> NoPlayer)
-    | hd :: tl -> check_line_aux board (match List.nth board.state hd with | PlayerX -> score -1 | PlayerO -> +1 | _ -> score) tl
+    match line with
+    | [] -> (
+        let result = score / board.row_size in
+        match abs result with 1 -> player_of_int result | _ -> NoPlayer)
+    | hd :: tl ->
+        let board_value = List.nth board.state hd in
+        let score = score + int_of_player board_value in
+        check_line_aux board score tl
 
- let check_line board line =
-     check_line_aux board 0 line
+  let check_line board line = check_line_aux board 0 line
 
- let rec check_lines_aux board lines results =
-     match lines with
-     | [] -> results
-     | hd::tl -> check_lines_aux board tl ((check_line board hd)::results)
+  let rec check_lines_aux board lines results =
+    match lines with
+    | [] -> results
+    | hd :: tl -> check_lines_aux board tl (check_line board hd :: results)
 
- let check_lines board lines =
-     check_lines_aux board lines []
+  let check_lines board lines = check_lines_aux board lines []
 
+  let is_game_over board =
+    let results =
+      compute_winning_lines board.row_size
+      |> check_lines board
+      |> List.filter (fun r -> r != NoPlayer)
+    in
+    List.nth_opt results 0
+end
 
-  let is_game_over board = 
-      let results = compute_winning_lines board.row_size |> check_lines board |> List.filter (fun r -> r != NoPlayer) in
-      List.nth_opt results 0
+let row_size = 3
 
-  let row_size = 3
-
-  let () =
-    let b = Board.new_ row_size in
-    print_newline ();
-    let b = Board.make_move 0 b in
-    print_int @@ List.length b.state;
-    print_newline ();
-    print_string @@ Board.show_board b
+let () =
+  let b = Board.new_ row_size in
+  print_newline ();
+  (* let b = Board.make_move 0 b in *)
+  let b =
+    b |> Board.make_move 0 |> Board.make_move 3 |> Board.make_move 1
+    |> Board.make_move 4 |> Board.make_move 2
+  in
+  print_newline ();
+  print_string @@ Board.show_board b;
+  print_newline ();
+  let result = Board.is_game_over b in
+  print_string
+    (match result with
+    | None -> "game in progress"
+    | Some v -> Board.show_player v);
+  print_newline ()
